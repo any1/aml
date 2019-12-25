7b4b7add73efda5ca5e5745b0ee1fbbc4bef4d67
Andri Yngvason <andri@yngvason.is>
2019-12-25 17:35:40 +0000

    Create project for a simple main loop library

---
+++ include/aml.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stdint.h>
+
+struct aml;
+struct aml_handler;
+struct aml_timer;
+struct aml_ticker;
+
+struct aml_backend {
+	void* (*new_state)(void);
+	void (*del_state)(void* state);
+	int (*poll)(void* state, int timeout);
+	int (*add_fd)(void* state, int fd, uint32_t event_mask);
+	int (*mod_fd)(void* state, int fd, uint32_t event_mask);
+	int (*del_fd)(void* state, int fd);
+};
+
+typedef void (*aml_callback_fn)(void* obj);
+
+/* Create a new main loop instance */
+struct aml* aml_new(const struct aml_backend*);
+
+/* Get/set the default main loop instance */
+void aml_set_default(struct aml*);
+struct aml* aml_get_default(void);
+
+int aml_run_once(struct aml*);
+
+int aml_run(struct aml*);
+void aml_exit(struct aml*);
+
+void aml_ref(void* obj);
+void aml_unref(void* obj);
+
+struct aml_handler* aml_handler_new(int fd, aml_callback_fn, void* userdata);
+
+struct aml_timer* aml_timer_new(uint32_t timeout, aml_callback_fn,
+                                void* userdata);
+
+struct aml_ticker* aml_ticker_new(uint32_t period, aml_callback_fn,
+                                  void* userdata);
+
+int aml_get_fd(const void* obj);
+
+void aml_set_userdata(void* obj, void* userdata);
+void* aml_get_userdata(const void* obj);
+
+int aml_start(struct aml*, void* obj);
+int aml_stop(void* obj);
---
+++ src/posix.c
@@ -0,0 +1,110 @@
+#include <poll.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "aml.h"
+
+struct posix_state {
+	struct pollfd* fds;
+
+	uint32_t max_fds;
+	uint32_t num_fds;
+};
+
+static void* posix_new_state(void)
+{
+	struct posix_state* self = calloc(1, sizeof(*self));
+	if (!self)
+		return NULL;
+
+	self->max_fds = 128;
+	self->fds = malloc(sizeof(*self->fds) * self->max_fds);
+	if (!self->fds)
+		goto failure;
+
+	return self;
+
+failure:
+	free(self);
+	return NULL;
+}
+
+static int posix__find_fd(struct posix_state* self, int fd)
+{
+	for (int i = 0; i < self->num_fds; ++i)
+		if (self->fds[i].fd == fd)
+			return i;
+
+	return -1;
+}
+
+void posix_del_state(void* state)
+{
+	struct posix_state* self = state;
+
+	free(self);
+}
+
+int posix_poll(void* state, int timeout)
+{
+	struct posix_state* self = state;
+
+	return poll(self->fds, self->num_fds, timeout);
+}
+
+int posix_add_fd(void* state, int fd, uint32_t event_mask)
+{
+	struct posix_state* self = state;
+
+	if (self->num_fds >= self->max_fds) {
+		uint32_t new_max = self->max_fds * 2;
+		struct pollfd* fds = realloc(self->fds, sizeof(*fds) * new_max);
+		if (!fds)
+			return -1;
+
+		self->fds = fds;
+		self->max_fds = new_max;
+	}
+
+	struct pollfd* event = &self->fds[self->num_fds++];
+	event->events = event_mask;
+	event->revents = 0;
+	event->fd = fd;
+
+	return 0;
+}
+
+int posix_mod_fd(void* state, int fd, uint32_t event_mask)
+{
+	struct posix_state* self = state;
+
+	int index = posix__find_fd(self, fd);
+	if (index < 0)
+		return -1;
+
+	self->fds[index].events = event_mask;
+
+	return 0;
+}
+
+int posix_del_fd(void* state, int fd)
+{
+	struct posix_state* self = state;
+
+	int index = posix__find_fd(self, fd);
+	if (index < 0)
+		return -1;
+
+	self->fds[index] = self->fds[--self->num_fds];
+
+	return 0;
+}
+
+const struct aml_backend posix_backend = {
+	.new_state = posix_new_state,
+	.del_state = posix_del_state,
+	.poll = posix_poll,
+	.add_fd = posix_add_fd,
+	.mod_fd = posix_mod_fd,
+	.del_fd = posix_del_fd,
+};
