7791cab972d955c38187d2d7d86c8d6f744ad66d
jaunty wunder kind <jaunty@eldergods.com>
2021-10-11 02:21:15 -0400

    debianization of libaml0, libaml-dev

---
+++ debian/libaml.doc-base.ex
@@ -0,0 +1,20 @@
+Document: libaml
+Title: Debian libaml Manual
+Author: <insert document author here>
+Abstract: This manual describes what libaml is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/libaml/libaml.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/libaml/libaml.ps.gz
+
+Format: text
+Files: /usr/share/doc/libaml/libaml.text.gz
+
+Format: HTML
+Index: /usr/share/doc/libaml/html/index.html
+Files: /usr/share/doc/libaml/html/*.html
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibaml 0.2.0 libaml (>> 0.2.0-0), libaml (<< 0.2.0-99)
