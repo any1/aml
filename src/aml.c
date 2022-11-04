any1/aml

commit fb2382cb491a7a8b70dfb41dc7371a49230d56b5

Don't run callbacks for stopped objects

src/aml.c
@@ -508,6 +508,18 @@ struct aml_idle* aml_idle_new(aml_callback_fn callback, void* userdata,
 	return self;
 }
 
+static bool aml__obj_is_single_shot(void* ptr)
+{
+	struct aml_obj* obj = ptr;
+	switch (obj->type) {
+	case AML_OBJ_TIMER: /* fallthrough */
+	case AML_OBJ_WORK:
+		return true;
+	default:;
+	}
+	return false;
+}
+
 static bool aml__obj_is_started_unlocked(struct aml* self, void* obj)
 {
 	struct aml_obj* elem;
@@ -593,7 +605,6 @@ static int aml__start_timer(struct aml* self, struct aml_timer* timer)
 
 	if (timer->timeout == 0) {
 		assert(timer->obj.type != AML_OBJ_TICKER);
-		aml_stop(self, timer);
 		aml_emit(self, timer, 0);
 		aml_interrupt(self);
 		return 0;
@@ -679,7 +690,6 @@ static int aml__stop_signal(struct aml* self, struct aml_signal* sig)
 
 static int aml__stop_work(struct aml* self, struct aml_work* work)
 {
-	/* Note: The cb may be executed anyhow */
 	return 0;
 }
 
@@ -749,7 +759,6 @@ static bool aml__handle_timeout(struct aml* self, uint64_t now)
 
 	switch (timer->obj.type) {
 	case AML_OBJ_TIMER:
-		aml_stop(self, timer);
 		break;
 	case AML_OBJ_TICKER:
 		timer->deadline += timer->timeout;
@@ -767,7 +776,7 @@ static void aml__handle_idle(struct aml* self)
 	struct aml_idle* idle;
 
 	LIST_FOREACH(idle, &self->idle_list, link)
-		if (idle->obj.cb)
+		if (idle->obj.cb && aml_is_started(self, idle))
 			idle->obj.cb(idle);
 }
 
@@ -778,8 +787,15 @@ static void aml__handle_event(struct aml* self, struct aml_obj* obj)
 	 */
 	aml_ref(obj);
 
-	if (obj->cb)
+	if (obj->cb && aml_is_started(self, obj)) {
+		/* Single-shot objects must be stopped before the callback so
+		 * that they can be restarted from within the callback.
+		 */
+		if (aml__obj_is_single_shot(obj))
+			aml_stop(self, obj);
+
 		obj->cb(obj);
+	}
 
 	if (obj->type == AML_OBJ_HANDLER) {
 		struct aml_handler* handler = (struct aml_handler*)obj;

src/thread-pool.c
@@ -112,7 +112,6 @@ static void* worker_fn(void* context)
 			aml_weak_ref_read(work->aml_ref) : NULL;
 		if (aml) {
 			aml_emit(aml, work->work, 0);
-			aml_stop(aml, work->work);
 			aml_interrupt(aml);
 			aml_unref(aml);
 		}

commit ba8799844352cbe0aa574d0f9ae7de0d343f8302

Mark expired one-shot timers as expired
Otherwise, they will be processed again...

This fixes a bug introduced in fb2382cb491a7a8b70dfb41dc7371a49230d56b5

src/aml.c
@@ -91,6 +91,7 @@ struct aml_timer {
 
 	uint64_t timeout;
 	uint64_t deadline;
+	bool expired;
 
 	LIST_ENTRY(aml_timer) link;
 };
@@ -598,6 +599,7 @@ static int aml__start_handler(struct aml* self, struct aml_handler* handler)
 static int aml__start_timer(struct aml* self, struct aml_timer* timer)
 {
 	timer->deadline = aml__gettime_us(self) + timer->timeout;
+	timer->expired = false;
 
 	pthread_mutex_lock(&self->timer_list_mutex);
 	LIST_INSERT_HEAD(&self->timer_list, timer, link);
@@ -740,7 +742,7 @@ static struct aml_timer* aml__get_timer_with_earliest_deadline(struct aml* self)
 
 	pthread_mutex_lock(&self->timer_list_mutex);
 	LIST_FOREACH(timer, &self->timer_list, link)
-		if (timer->deadline < deadline) {
+		if (!timer->expired && timer->deadline < deadline) {
 			deadline = timer->deadline;
 			result = timer;
 		}
@@ -759,6 +761,7 @@ static bool aml__handle_timeout(struct aml* self, uint64_t now)
 
 	switch (timer->obj.type) {
 	case AML_OBJ_TIMER:
+		timer->expired = true;
 		break;
 	case AML_OBJ_TICKER:
 		timer->deadline += timer->timeout;